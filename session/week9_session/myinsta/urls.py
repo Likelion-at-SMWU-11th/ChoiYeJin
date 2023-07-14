ca6937843483f9c9b5cdd3ec99e4e82ff73b2f8e
Choi YeJin <amy350500@gmail.com>
2023-07-14 21:47:34 +0900

    4. Django Templates Engine 완료

--- session/week9_session/myinsta/urls.py
+++ session/week9_session/myinsta/urls.py
@@ -1,23 +1,7 @@
-"""
-URL configuration for myinsta project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
-from posts.views import url_view, url_parameter_view, function_view
+from posts.views import url_view, url_parameter_view, function_view, index
 from posts.views import class_view
 
 urlpatterns = [
@@ -28,4 +12,7 @@ urlpatterns = [
     path('fbv/', function_view),
     # Class Based View
     path('cbv/', class_view.as_view()), # as_view: 진입 메소드
+
+    path('', index, name='index'),
+    path('posts/', include('posts.urls', namespace='posts')),
 ]
