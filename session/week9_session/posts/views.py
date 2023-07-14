85f147b1988f22fb79877dcde0f25928eaf95a48
Choi YeJin <amy350500@gmail.com>
2023-07-14 17:54:17 +0900

    2. Function Based View 완료

---
+++ session/week9_session/posts/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+
+# Create your views here.
+def url_view(request):
+    data = {'code': '001', 'msg': 'OK'}
+    return HttpResponse('<h1>url_views</h1>')
+
+def url_parameter_view(request, username):
+    print(f'url_parameter_view()')
+    print(f'username: {username}')
+    print(f'request.GET: {request.GET}')
+    return HttpResponse(username)
+
+def function_view(request):
+    print(f'request.method: {request.method}')
+
+    if request.method == "GET":
+        print(f'request.GET: {request.GET}')
+    elif request.method == 'POST':
+        print(f'request.POST: {request.POST}')
+    return render(request, 'view.html')
