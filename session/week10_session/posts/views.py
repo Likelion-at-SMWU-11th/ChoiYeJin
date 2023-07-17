80c0be0572c564efda853c61dd5785dbbffbf868
Choi YeJin <amy350500@gmail.com>
2023-07-17 15:35:47 +0900

    Create로 데이터 생성하기

--- session/week10_session/posts/views.py
+++ session/week10_session/posts/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse, JsonResponse
-
 from django.views.generic import ListView
+from django.contrib.auth.decorators import login_required
+
 from .models import Post
 
 def index(request):
@@ -13,8 +14,21 @@ def post_list_view(request):
 def post_detail_view(request, id):
     return render(request, 'posts/post_detail.html')
 
+@login_required
 def post_create_view(request):
-    return render(request, 'posts/post_form.html')
+    if request.method == 'GET':
+        return render(request, 'posts/post_form.html')
+    else:
+        image = request.FILES.get('image') 
+        content = request.POST.get('content')
+        print(image)
+        print(content)
+        Post.objects.create( # image, content 데이터를 담은 Post 객체 만들어서 저장
+            image = image,
+            content = content,
+            writer = request.user
+        )
+        return redirect('index')
 
 def post_update_view(request, id):
     return render(request, 'posts/post_update.html')
